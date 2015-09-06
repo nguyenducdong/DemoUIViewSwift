9fe386f727e052044228f092d1df00f45cdbad35
bsv-DongNguyen <dongnguyenduc@Dongs-MacBook-Pro.local>
2015-09-06 21:28:29 +0700

    Initial Commit

---
+++ DongPracticeObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DongPracticeObjectiveC
+//
+//  Created by Dong Nguyen Duc on 9/6/15.
+//  Copyright (c) 2015 Dong Nguyen Duc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DongPracticeObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DongPracticeObjectiveC
+//
+//  Created by Dong Nguyen Duc on 9/6/15.
+//  Copyright (c) 2015 Dong Nguyen Duc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
